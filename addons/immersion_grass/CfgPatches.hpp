bbf4aa00144b816e74230ef4f551a594cf7845cb
Tom Campbell <tyen901@gmail.com>
2025-09-19 20:04:55 +1200

    fix(addons): update requiredVersion to 2.0 in CfgPatches for immersion_grass addon

--- addons/immersion_grass/CfgPatches.hpp
+++ addons/immersion_grass/CfgPatches.hpp
@@ -5,7 +5,7 @@ class CfgPatches
 		units[] = {"immersion_grass_jointpackItem"};
 		weapons[] = {};
 		magazines[] = {"immersion_grass_jointpack"};
-		requiredVersion = 0.1;
+		requiredVersion = 2.0;
 		requiredAddons[] = {"a3_modules_f", "a3_data_f"};
 	};
 };
