9372c7849864c482d62309d1e97019fa2850b78e
Tom Campbell <tyen901@gmail.com>
2025-09-19 21:08:42 +1200

    gentlemen

--- addons/immersion_cigs_wholepack/config.cpp
+++ addons/immersion_cigs_wholepack/config.cpp
@@ -8,8 +8,8 @@ class CfgGlasses
     {
         scope = 2;
         displayName = "Cigarettes (The Whole Pack)";
-        model = "\murshun_cigs\cigs0.p3d";
-        picture = "\murshun_cigs\UI\gear_cigpack_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs0.p3d";
+        picture = "murshun_cigs\UI\gear_cigpack_x_ca.paa";
         immersion_cigs_type = "wholepack";
         immersion_cigs_maxTime = 1320; // longer burn overall
         immersion_cigs_initStateTime = 0;
@@ -21,8 +21,8 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 1)";
-        model = "\murshun_cigs\cigs1.p3d";
-        picture = "\murshun_cigs\UI\gear_cig1_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs1.p3d";
+        picture = "murshun_cigs\UI\gear_cig1_x_ca.paa";
         immersion_cigs_initStateTime = 12;
         immersion_cigs_nextState = "immersion_wholepack_cig2";
         immersion_cigs_isCig = 1;
@@ -32,8 +32,8 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 2)";
-        model = "\murshun_cigs\cigs2.p3d";
-        picture = "\murshun_cigs\UI\gear_cig2_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs2.p3d";
+        picture = "murshun_cigs\UI\gear_cig2_x_ca.paa";
         immersion_cigs_initStateTime = 132;
         immersion_cigs_nextState = "immersion_wholepack_cig3";
         immersion_cigs_isCig = 1;
@@ -43,8 +43,8 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 3)";
-        model = "\murshun_cigs\cigs3.p3d";
-        picture = "\murshun_cigs\UI\gear_cig3_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs3.p3d";
+        picture = "murshun_cigs\UI\gear_cig3_x_ca.paa";
         immersion_cigs_initStateTime = 252;
         immersion_cigs_nextState = "immersion_wholepack_cig4";
         immersion_cigs_isCig = 1;
@@ -54,8 +54,8 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 4)";
-        model = "\murshun_cigs\cigs4.p3d";
-        picture = "\murshun_cigs\UI\gear_cig4_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs4.p3d";
+        picture = "murshun_cigs\UI\gear_cig4_x_ca.paa";
         immersion_cigs_initStateTime = 612;
         immersion_cigs_nextState = "";
         immersion_cigs_isCig = 1;
@@ -70,8 +70,8 @@ class CfgWeapons
     {
         scope = 2;
         displayName = "Cigarettes (The Whole Pack) (NVG Slot)";
-        model = "\murshun_cigs\cigs0.p3d";
-        picture = "\murshun_cigs\UI\gear_cigpack_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs0.p3d";
+        picture = "murshun_cigs\UI\gear_cigpack_x_ca.paa";
         visionMode[] = {"Normal"};
         thermalMode[] = {0};
         modelOptics[] = {};
@@ -85,8 +85,8 @@ class CfgWeapons
         {
             type = 616;
             hmdType = 0;
-            uniformModel = "\murshun_cigs\cigs0.p3d";
-            modelOff = "\murshun_cigs\cigs0.p3d";
+            uniformModel = "immersion_cigs_wholepack\cigs0.p3d";
+            modelOff = "immersion_cigs_wholepack\cigs0.p3d";
         };
     };
 
@@ -94,8 +94,8 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 1) (NVG Slot)";
-        model = "\murshun_cigs\cigs1.p3d";
-        picture = "\murshun_cigs\UI\gear_cig1_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs1.p3d";
+        picture = "murshun_cigs\UI\gear_cig1_x_ca.paa";
         immersion_cigs_initStateTime = 12;
         immersion_cigs_nextState = "immersion_wholepack_cig2_nv";
         immersion_cigs_isCig = 1;
@@ -105,8 +105,8 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 2) (NVG Slot)";
-        model = "\murshun_cigs\cigs2.p3d";
-        picture = "\murshun_cigs\UI\gear_cig2_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs2.p3d";
+        picture = "murshun_cigs\UI\gear_cig2_x_ca.paa";
         immersion_cigs_initStateTime = 132;
         immersion_cigs_nextState = "immersion_wholepack_cig3_nv";
         immersion_cigs_isCig = 1;
@@ -116,8 +116,8 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 3) (NVG Slot)";
-        model = "\murshun_cigs\cigs3.p3d";
-        picture = "\murshun_cigs\UI\gear_cig3_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs3.p3d";
+        picture = "murshun_cigs\UI\gear_cig3_x_ca.paa";
         immersion_cigs_initStateTime = 252;
         immersion_cigs_nextState = "immersion_wholepack_cig4_nv";
         immersion_cigs_isCig = 1;
@@ -127,8 +127,8 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 4) (NVG Slot)";
-        model = "\murshun_cigs\cigs4.p3d";
-        picture = "\murshun_cigs\UI\gear_cig4_x_ca.paa";
+        model = "immersion_cigs_wholepack\cigs4.p3d";
+        picture = "murshun_cigs\UI\gear_cig4_x_ca.paa";
         immersion_cigs_initStateTime = 612;
         immersion_cigs_nextState = "";
         immersion_cigs_isCig = 1;
