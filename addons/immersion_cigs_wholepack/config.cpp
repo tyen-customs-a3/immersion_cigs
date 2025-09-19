9abb1c65624fad10653ccdbc52a6dcc53b733e0d
Tom Campbell <tyen901@gmail.com>
2025-09-19 20:26:12 +1200

    feat(addons): add new cigarette models and update references in config

--- addons/immersion_cigs_wholepack/config.cpp
+++ addons/immersion_cigs_wholepack/config.cpp
@@ -8,7 +8,7 @@ class CfgGlasses
     {
         scope = 2;
         displayName = "Cigarettes (The Whole Pack)";
-        model = "\murshun_cigs\cig0.p3d";
+        model = "\murshun_cigs\cigs0.p3d";
         picture = "\murshun_cigs\UI\gear_cigpack_x_ca.paa";
         immersion_cigs_type = "wholepack";
         immersion_cigs_maxTime = 1320; // longer burn overall
@@ -21,7 +21,7 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 1)";
-        model = "\murshun_cigs\cig1.p3d";
+        model = "\murshun_cigs\cigs1.p3d";
         picture = "\murshun_cigs\UI\gear_cig1_x_ca.paa";
         immersion_cigs_initStateTime = 12;
         immersion_cigs_nextState = "immersion_wholepack_cig2";
@@ -32,7 +32,7 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 2)";
-        model = "\murshun_cigs\cig2.p3d";
+        model = "\murshun_cigs\cigs2.p3d";
         picture = "\murshun_cigs\UI\gear_cig2_x_ca.paa";
         immersion_cigs_initStateTime = 132;
         immersion_cigs_nextState = "immersion_wholepack_cig3";
@@ -43,7 +43,7 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 3)";
-        model = "\murshun_cigs\cig3.p3d";
+        model = "\murshun_cigs\cigs3.p3d";
         picture = "\murshun_cigs\UI\gear_cig3_x_ca.paa";
         immersion_cigs_initStateTime = 252;
         immersion_cigs_nextState = "immersion_wholepack_cig4";
@@ -54,7 +54,7 @@ class CfgGlasses
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 4)";
-        model = "\murshun_cigs\cig4.p3d";
+        model = "\murshun_cigs\cigs4.p3d";
         picture = "\murshun_cigs\UI\gear_cig4_x_ca.paa";
         immersion_cigs_initStateTime = 612;
         immersion_cigs_nextState = "";
@@ -70,7 +70,7 @@ class CfgWeapons
     {
         scope = 2;
         displayName = "Cigarettes (The Whole Pack) (NVG Slot)";
-        model = "\murshun_cigs\cig0.p3d";
+        model = "\murshun_cigs\cigs0.p3d";
         picture = "\murshun_cigs\UI\gear_cigpack_x_ca.paa";
         visionMode[] = {"Normal"};
         thermalMode[] = {0};
@@ -85,8 +85,8 @@ class CfgWeapons
         {
             type = 616;
             hmdType = 0;
-            uniformModel = "\murshun_cigs\cig0.p3d";
-            modelOff = "\murshun_cigs\cig0.p3d";
+            uniformModel = "\murshun_cigs\cigs0.p3d";
+            modelOff = "\murshun_cigs\cigs0.p3d";
         };
     };
 
@@ -94,7 +94,7 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 1) (NVG Slot)";
-        model = "\murshun_cigs\cig1.p3d";
+        model = "\murshun_cigs\cigs1.p3d";
         picture = "\murshun_cigs\UI\gear_cig1_x_ca.paa";
         immersion_cigs_initStateTime = 12;
         immersion_cigs_nextState = "immersion_wholepack_cig2_nv";
@@ -105,7 +105,7 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 2) (NVG Slot)";
-        model = "\murshun_cigs\cig2.p3d";
+        model = "\murshun_cigs\cigs2.p3d";
         picture = "\murshun_cigs\UI\gear_cig2_x_ca.paa";
         immersion_cigs_initStateTime = 132;
         immersion_cigs_nextState = "immersion_wholepack_cig3_nv";
@@ -116,7 +116,7 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 3) (NVG Slot)";
-        model = "\murshun_cigs\cig3.p3d";
+        model = "\murshun_cigs\cigs3.p3d";
         picture = "\murshun_cigs\UI\gear_cig3_x_ca.paa";
         immersion_cigs_initStateTime = 252;
         immersion_cigs_nextState = "immersion_wholepack_cig4_nv";
@@ -127,7 +127,7 @@ class CfgWeapons
     {
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 4) (NVG Slot)";
-        model = "\murshun_cigs\cig4.p3d";
+        model = "\murshun_cigs\cigs4.p3d";
         picture = "\murshun_cigs\UI\gear_cig4_x_ca.paa";
         immersion_cigs_initStateTime = 612;
         immersion_cigs_nextState = "";
