d8ca789634ed39ebec3e16541a46bb1cdbaa3c94
Tom Campbell <tyen901@gmail.com>
2025-09-21 20:59:00 +1200

    feat(addons): update immersion_cigs_wholepack with new models, improved smoke handling, and open pack functionality

--- addons/immersion_cigs_wholepack/config.cpp
+++ addons/immersion_cigs_wholepack/config.cpp
@@ -9,9 +9,9 @@ class CfgGlasses
         scope = 2;
         displayName = "Cigarettes (The Whole Pack)";
         model = "immersion_cigs_wholepack\cigs0.p3d";
-        picture = "murshun_cigs\UI\gear_cigpack_x_ca.paa";
+        picture = "\murshun_cigs\UI\gear_cigpack_x_ca.paa";
         immersion_cigs_type = "wholepack";
-        immersion_cigs_maxTime = 1320; // longer burn overall
+        immersion_cigs_maxTime = 495; // 1.5x longer burn than a single cigarette
         immersion_cigs_initStateTime = 0;
         immersion_cigs_nextState = "immersion_wholepack_cig1";
         immersion_cigs_isCig = 1;
@@ -22,8 +22,8 @@ class CfgGlasses
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 1)";
         model = "immersion_cigs_wholepack\cigs1.p3d";
-        picture = "murshun_cigs\UI\gear_cig1_x_ca.paa";
-        immersion_cigs_initStateTime = 12;
+        picture = "\murshun_cigs\UI\gear_cig1_x_ca.paa";
+        immersion_cigs_initStateTime = 9;
         immersion_cigs_nextState = "immersion_wholepack_cig2";
         immersion_cigs_isCig = 1;
     };
@@ -33,8 +33,8 @@ class CfgGlasses
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 2)";
         model = "immersion_cigs_wholepack\cigs2.p3d";
-        picture = "murshun_cigs\UI\gear_cig2_x_ca.paa";
-        immersion_cigs_initStateTime = 132;
+        picture = "\murshun_cigs\UI\gear_cig2_x_ca.paa";
+        immersion_cigs_initStateTime = 99;
         immersion_cigs_nextState = "immersion_wholepack_cig3";
         immersion_cigs_isCig = 1;
     };
@@ -44,8 +44,8 @@ class CfgGlasses
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 3)";
         model = "immersion_cigs_wholepack\cigs3.p3d";
-        picture = "murshun_cigs\UI\gear_cig3_x_ca.paa";
-        immersion_cigs_initStateTime = 252;
+        picture = "\murshun_cigs\UI\gear_cig3_x_ca.paa";
+        immersion_cigs_initStateTime = 189;
         immersion_cigs_nextState = "immersion_wholepack_cig4";
         immersion_cigs_isCig = 1;
     };
@@ -55,8 +55,8 @@ class CfgGlasses
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 4)";
         model = "immersion_cigs_wholepack\cigs4.p3d";
-        picture = "murshun_cigs\UI\gear_cig4_x_ca.paa";
-        immersion_cigs_initStateTime = 612;
+        picture = "\murshun_cigs\UI\gear_cig4_x_ca.paa";
+        immersion_cigs_initStateTime = 459;
         immersion_cigs_nextState = "";
         immersion_cigs_isCig = 1;
     };
@@ -71,13 +71,13 @@ class CfgWeapons
         scope = 2;
         displayName = "Cigarettes (The Whole Pack) (NVG Slot)";
         model = "immersion_cigs_wholepack\cigs0.p3d";
-        picture = "murshun_cigs\UI\gear_cigpack_x_ca.paa";
+        picture = "\murshun_cigs\UI\gear_cigpack_x_ca.paa";
         visionMode[] = {"Normal"};
         thermalMode[] = {0};
         modelOptics[] = {};
         opticsPPEffects[] = {};
         immersion_cigs_type = "wholepack";
-        immersion_cigs_maxTime = 1320;
+        immersion_cigs_maxTime = 495;
         immersion_cigs_initStateTime = 0;
         immersion_cigs_nextState = "immersion_wholepack_cig1_nv";
         immersion_cigs_isCig = 1;
@@ -95,8 +95,8 @@ class CfgWeapons
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 1) (NVG Slot)";
         model = "immersion_cigs_wholepack\cigs1.p3d";
-        picture = "murshun_cigs\UI\gear_cig1_x_ca.paa";
-        immersion_cigs_initStateTime = 12;
+        picture = "\murshun_cigs\UI\gear_cig1_x_ca.paa";
+        immersion_cigs_initStateTime = 9;
         immersion_cigs_nextState = "immersion_wholepack_cig2_nv";
         immersion_cigs_isCig = 1;
     };
@@ -106,8 +106,8 @@ class CfgWeapons
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 2) (NVG Slot)";
         model = "immersion_cigs_wholepack\cigs2.p3d";
-        picture = "murshun_cigs\UI\gear_cig2_x_ca.paa";
-        immersion_cigs_initStateTime = 132;
+        picture = "\murshun_cigs\UI\gear_cig2_x_ca.paa";
+        immersion_cigs_initStateTime = 99;
         immersion_cigs_nextState = "immersion_wholepack_cig3_nv";
         immersion_cigs_isCig = 1;
     };
@@ -117,8 +117,8 @@ class CfgWeapons
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 3) (NVG Slot)";
         model = "immersion_cigs_wholepack\cigs3.p3d";
-        picture = "murshun_cigs\UI\gear_cig3_x_ca.paa";
-        immersion_cigs_initStateTime = 252;
+        picture = "\murshun_cigs\UI\gear_cig3_x_ca.paa";
+        immersion_cigs_initStateTime = 189;
         immersion_cigs_nextState = "immersion_wholepack_cig4_nv";
         immersion_cigs_isCig = 1;
     };
@@ -128,9 +128,56 @@ class CfgWeapons
         scope = 1;
         displayName = "Cigarettes (The Whole Pack) (Stage 4) (NVG Slot)";
         model = "immersion_cigs_wholepack\cigs4.p3d";
-        picture = "murshun_cigs\UI\gear_cig4_x_ca.paa";
-        immersion_cigs_initStateTime = 612;
+        picture = "\murshun_cigs\UI\gear_cig4_x_ca.paa";
+        immersion_cigs_initStateTime = 459;
         immersion_cigs_nextState = "";
         immersion_cigs_isCig = 1;
     };
 };
+class CfgVehicles
+{
+    class Man;
+    class CAManBase : Man
+    {
+        class ACE_SelfActions
+        {
+            class ACE_Equipment
+            {
+                class immersion_cigs_open_wholepack
+                {
+                    displayName = "Open whole pack";
+                    condition = "[_player] call immersion_cigs_wholepack_fnc_canOpenWholePack";
+                    statement = "[_player] spawn immersion_cigs_wholepack_fnc_openWholePack";
+                    showDisabled = 0;
+                    exceptions[] = {"isNotInside", "isNotSitting"};
+                    icon = "murshun_cigs\UI\take_cig.paa";
+                };
+            };
+        };
+    };
+};
+class CfgFunctions
+{
+    class immersion_cigs_wholepack
+    {
+        class functions
+        {
+            file = "immersion_cigs_wholepack\functions";
+            class postInit
+            {
+                postInit = 1;
+                recompile = 1;
+            };
+        };
+    };
+};
+
+
+
+
+
+
+
+
+
+
--- addons/immersion_cigs_wholepack/functions/fn_postInit.sqf
+++ addons/immersion_cigs_wholepack/functions/fn_postInit.sqf
@@ -1,23 +1,78 @@
+// Extend murshun_cigs smoke handling for the wholepack variant
+if (isNil "immersion_cigs_wholepack_fnc_originalSmoke" && {!isNil "murshun_cigs_fnc_smoke"}) then {
+    immersion_cigs_wholepack_fnc_originalSmoke = murshun_cigs_fnc_smoke;
+
+    murshun_cigs_fnc_smoke = {
+        params ["_unit", "_type"];
+
+        if (_type == "wholepack") exitWith {
+            private _delays = [0, 0.4, 0.8];
+
+            {
+                [_unit, _x] spawn {
+                    params ["_unit", "_delay"];
+
+                    sleep _delay;
+
+                    for "_i" from 1 to 3 do {
+                        [_unit, "wholepack"] spawn immersion_cigs_wholepack_fnc_originalSmoke;
+                        sleep 0.01;
+                    };
+                };
+            } forEach _delays;
+
+            true
+        };
+
+        [_unit, _type] call immersion_cigs_wholepack_fnc_originalSmoke;
+    };
+};
+
 if (!hasInterface) exitWith {};
 
-private _addVanillaActions_wholepack = {
-    player addAction ["Open The Entire Pack", {
+immersion_cigs_wholepack_fnc_canOpenWholePack = {
+    params ["_unit"];
+
+    if (isNil "immersion_cigs_canTakeCigFromPack") exitWith {false};
+
+    [_unit] call immersion_cigs_canTakeCigFromPack;
+};
+
+immersion_cigs_wholepack_fnc_openWholePack = {
+    params ["_player"];
+
+    if (!([_player] call immersion_cigs_wholepack_fnc_canOpenWholePack)) exitWith {};
+
+    private _packClass = "murshun_cigs_cigpack";
+    private _hasPack = magazinesAmmo _player select {(_x select 0) == _packClass};
+
+    if (_hasPack isEqualTo []) exitWith {};
+
+    private _newItem = "immersion_wholepack_cig0";
+
+    if (goggles _player != "") then {
+        if (hmd _player == "") then {
+            _newItem = "immersion_wholepack_cig0_nv";
+        };
+    };
+
+    if (!(_player canAdd _newItem)) exitWith {
+        ["Not enough space to open the whole pack.", 2.5, _player] spawn ace_common_fnc_displayTextStructured;
+    };
+
+    _player removeMagazineGlobal _packClass;
+    [_player, "murshun_cigs_unwrap"] call murshun_cigs_playSound;
+
+    _player addItem _newItem;
+};
+
+private _addVanillaActions = {
+    player addAction ["Open whole pack", {
         params ["_target", "_caller"];
-        [_caller] spawn immersion_wholepack_fnc_open_pack
-    }, nil, 0, false, true, "", "if (_target != player) exitWith {false}; 'murshun_cigs_cigpack' in (magazineCargo uniformContainer player) || 'murshun_cigs_cigpack' in (magazineCargo vestContainer player)", 5, false];
+        [_caller] spawn immersion_cigs_wholepack_fnc_openWholePack;
+    }, nil, 0, false, true, "", "if (_target != player) exitWith {false}; [_target] call immersion_cigs_wholepack_fnc_canOpenWholePack", 5, false];
 };
 
-if !(isClass (configFile >> "CfgPatches" >> "ace_interact_menu")) then {
-    call _addVanillaActions_wholepack;
+call _addVanillaActions;
 
-    player addEventHandler ["Respawn", _addVanillaActions_wholepack];
-} else {
-    private _actionOpen = ["immersion_wholepack_open_pack", "Open The Entire Pack", "murshun_cigs\\UI\\gear_cigpack_x_ca.paa", {
-        params ["_target", "_player"];
-        [_player] spawn immersion_wholepack_fnc_open_pack
-    }, {
-        params ["_target", "_player"];
-        'murshun_cigs_cigpack' in (magazineCargo _player) || 'murshun_cigs_cigpack' in (magazineCargo _player)
-    }] call ace_interact_menu_fnc_createAction;
-    ["CAManBase", 0, ["ACE_Head"], _actionOpen, true] call ace_interact_menu_fnc_addActionToClass;
-};
+player addEventHandler ["Respawn", _addVanillaActions];
